616abb7bff4d6aadbec502a706528f7ef749c855
hp victus <hp victus@LAPTOP-2I0QRUOT>
2024-04-03 00:58:26 +0300

    Добавьте файлы проекта.

---
+++ Lesson 2.3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int totalMoney = 8005234;
+    int numderOfEntrances = 6;
+    int numberOfApartments = 30;
+    int totalApartments = numderOfEntrances * numberOfApartments;
+
+
+
+    cout << "Приветствуем вас в калькуляторе квартплаты!\n";
+    cout << "\nВведите сумму, указанную в квитанции : " << totalMoney;
+    cout << "\nСколько подъездов в вашем доме? " << numderOfEntrances;
+    cout << "\nСколько квартир в каждом подъезде? " << numberOfApartments;
+    cout << "\n-----Считаем-----";
+    cout << "\nКаждая квартира должна платить по " << totalMoney / totalApartments << " руб"<< "\n";
+
+}
